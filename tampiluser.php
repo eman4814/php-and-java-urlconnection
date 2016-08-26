537cf232bbe41ff6c5aa3d216a46e6e82a0a60ed
eman4814 <eman4814@gmail.com>
2016-08-26 16:02:34 +0700

    insert user

---
+++ tampiluser.php
@@ -0,0 +1,14 @@
+<?php 
+	require 'koneksi.php';
+
+	$sql="select * from user";
+	$result=$con -> query($sql);
+
+	if($result->num_rows > 0){
+		while($row = $result->fetch_assoc()){
+			$data[] = $row;
+		}
+		print json_encode($data);
+	}
+
+ ?>
